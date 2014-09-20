43c7cd4a46c226d1c852d9fbf6b1093c788681b2
Anton Khorev <tony29@yandex.ru>
2014-09-20 20:36:48 +0400

    activities

--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,33 @@
+library(dplyr)
+
 # filename='getdata_projectfiles_UCI HAR Dataset.zip'
 datasetDirName='UCI HAR Dataset'
 
 features=read.table(
-	paste(datasetDirName,'features.txt',sep='/'),
+	file.path(datasetDirName,'features.txt'),
 	col.names=c('FeatureIndex','FeatureName')
 )
 featureNames=gsub('\\(|\\)','',features$FeatureName)
 targetFeatures=grepl('-(mean|std)\\(\\)',features$FeatureName)
 
+activities=read.table(
+	file.path(datasetDirName,'activity_labels.txt'),
+	col.names=c('ActivityIndex','ActivityName')
+)
+activityNames=activities$ActivityName
+
 loadSet=function(dirName) {
 	cbind(
 		read.table(
-			paste(datasetDirName,dirName,'subject_test.txt',sep='/'),
+			file.path(datasetDirName,dirName,'subject_test.txt'),
 			col.names='Subject'
 		),
 		read.table(
-			paste(datasetDirName,dirName,'y_test.txt',sep='/'),
+			file.path(datasetDirName,dirName,'y_test.txt'),
 			col.names='Activity'
-		),
+		) %>% mutate(Activity=activityNames[Activity]),
 		read.table(
-			paste(datasetDirName,dirName,'X_test.txt',sep='/'),
+			file.path(datasetDirName,dirName,'X_test.txt'),
 			colClasses=ifelse(targetFeatures,NA,'NULL'),
 			col.names=featureNames
 		)
